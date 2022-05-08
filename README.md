fa83143fd008a586e4d4aacdcb0bd13975db9121
yueleng <wangyueleng@gmail.com>
2022-05-08 14:02:03 +0800

    tailwind-css-vite-react

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # tailwind-vite-react-main-page-demo
- 
+
+In this project, we used the following stacks.
+
+- tailwind css
+- vite
+- react.js
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // tailwind css 需要扫描的css文件用于编译tailwind css
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
